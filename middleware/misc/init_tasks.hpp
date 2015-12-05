bb2d6ce58bcc1c9915bb63119d3a9f38b5e202e8
Thomas Vanderbruggen <th.vanderbruggen@gmail.com>
2015-12-05 16:02:08 +0000

    Clean up

--- middleware/misc/init_tasks.hpp
+++ middleware/misc/init_tasks.hpp
@@ -1,23 +1,18 @@
-/// @file init_tasks.hpp
+/// Tasks to be performed at init. Callable via the CLI.
 ///
-/// @brief Task to be performed at init. Callable via the CLI.
-/// @date 12/09/2015
-///
-/// (c) Koheron 2014-2015
+/// (c) Koheron 
 
 #ifndef __MISC_INIT_HPP__
 #define __MISC_INIT_HPP__
 
 #include "../drivers/core/dev_mem.hpp"
 
-//> \description Task to be performed at init. Callable via the CLI.
 class InitTasks
 {
   public:
     InitTasks(Klib::DevMem& dev_mem_);
     
-    //> \description Display IP address last number onto the RedPitaya LEDs
-    //> \io_type WRITE
+    // Display IP address last number onto the RedPitaya LEDs
     void show_ip_on_leds();
     
   private:
