af9245511e5117f02280b4205023e3c5f85346e3
tvanderbruggen <th.vanderbruggen@gmail.com>
2015-12-26 16:10:03 +0000

    Add server relaunch script

---
+++ relaunch_kserver.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Duration between 2 consecutives checks
+check_interval=5
+
+# Check whether kserverd is started and start the server if it is down.
+#
+# https://www.digitalocean.com/community/tutorials/how-to-use-a-simple-bash-script-to-restart-server-programs
+_relaunch_kserver()
+{
+    ps auxw | grep kserverd | grep -v grep > /dev/null
+
+    if [ $? != 0 ]; then
+        /usr/local/tcp-server/kserverd -c /usr/local/tcp-server/kserver.conf > /dev/null
+    fi
+}
+
+# Check periodically
+while true ; do
+    echo "Check"
+    _relaunch_kserver&
+    sleep ${check_interval}
+done
