1963b5dae2b5a92cfb09c956ee173f9c02ad34c5
tvanderbruggen <th.vanderbruggen@gmail.com>
2016-03-08 23:53:17 +0000

    Add is_failed pragma

--- devgen/hpp_parser.py
+++ devgen/hpp_parser.py
@@ -2,16 +2,48 @@ import CppHeaderParser
     
 CSTR_TYPES = ["char *", "char*", "const char *", "const char*"]
 
+def ERROR(line, message):
+    raise ValueError("Error line " + str(line) + ": " + message)
+
 def parse_header(hpp_filename):
     try:
         cpp_header = CppHeaderParser.CppHeader(hpp_filename)
     except CppHeaderParser.CppParseError as e:
         print(e)
+        
+    pragmas = _get_pragmas(hpp_filename)
 
     devices = []
     for classname in cpp_header.classes:
-        devices.append(_get_device(cpp_header.classes[classname]))
+        devices.append(_get_device(cpp_header.classes[classname], pragmas))
+
     return devices
+
+def _get_pragmas(hpp_filename):
+    fd = open(hpp_filename, 'r')
+    line_cnt = 0
+    pragmas = []
+    for line in fd.readlines():
+        line_cnt = line_cnt + 1
+        if "# pragma" in line:
+            pragmas.append({
+              'line_number': line_cnt,
+              'data': line.split("# pragma")[1].strip()
+            })
+        elif "#pragma" in line:
+            pragmas.append({
+              'line_number': line_cnt,
+              'data': line.split("#pragma")[1].strip()
+            })
+    fd.close()
+    return pragmas
+
+def _get_method_pragma(method, pragmas):
+    if pragmas != None and len(pragmas) > 0:
+        for pragma in pragmas:
+            if pragma['line_number'] == method['line_number'] - 1:
+                return pragma
+    return None
 
 def _set_iotype(operation, _type):
     operation['io_type'] = {}
@@ -21,36 +53,60 @@ def _set_iotype(operation, _type):
         operation["io_type"] = {'value': 'READ_CSTR', 'remaining': ''}
     else:
         operation["io_type"] = {'value': 'READ', 'remaining': ''}
-    
-def _get_operation(method):
+
+def _get_operation(method, pragmas):
+    pragma = _get_method_pragma(method, pragmas)
     operation = {}
-    operation['prototype'] = {}
-    operation['prototype']['ret_type'] = method['rtnType']
+    operation['prototype'] = _get_operation_prototype(method)
     _set_iotype(operation, method['rtnType'])
-    operation['prototype']['name'] = method['name']
-    operation['prototype']['params'] = []
+    return operation
     
+def _get_operation_prototype(method):
+    prototype = {}
+    prototype['ret_type'] = method['rtnType']
+    prototype['name'] = method['name']
+    prototype['params'] = []
+
     for param in method['parameters']:
-        operation['prototype']['params'].append({
+        prototype['params'].append({
           'name': str(param['name']),
           'type': param['type']
         })
-    
-    return operation
-    
-def _get_device(_class):
+    return prototype
+
+def _get_is_failed(_class, pragmas):
+    for method in _class['methods']['public']:
+        pragma = _get_method_pragma(method, pragmas)
+        if pragma != None and pragma['data'] == 'is_failed':
+            prototype = _get_operation_prototype(method)
+            if prototype["ret_type"] != "bool":
+                ERROR(pragma['line_number'], 'Failure indicator function must return a bool')
+            if len(prototype['params']) > 0:
+                ERROR(pragma['line_number'], 'Failure indicator function must not have argument')
+            return prototype
+    return None
+        
+def _get_device(_class, pragmas):
     device = {}
     device["objects"] = [{
       "name": _class['name'].lower(),
       "type": str(_class['name'])
     }]
     device["name"] = _class['name']
+    
+    is_failed_data = _get_is_failed(_class, pragmas)
+    if is_failed_data != None:
+        device['is_failed'] = is_failed_data
+    
     device['operations'] = []
     for method in _class['methods']['public']:
         # We eliminate constructor and destructor
-        if method['name'] != _class['name'] and method['name'] != '~' + _class['name']:
-            device['operations'].append(_get_operation(method))
+        if method['name'] == _class['name'] or method['name'] == '~' + _class['name']:
+            continue
+            
+        if 'is_failed' in device and method['name'] == device['is_failed']['name']:
+            continue
+            
+        device['operations'].append(_get_operation(method, pragmas))
     return device
-
-    
 
--- middleware/tests/tests.hpp
+++ middleware/tests/tests.hpp
@@ -1,5 +1,3 @@
-/// (c) Koheron
-
 #ifndef __TESTS_TEST_HPP__
 #define __TESTS_TEST_HPP__
 
@@ -17,22 +15,17 @@ class Tests
     int Open(uint32_t waveform_size_);
     void Close();
 
-    std::vector<float>& read();
     void set_mean(float mean_);
     void set_std_dev(float mean_);
-
+    std::vector<float>& read();
     std::array<uint32_t, 10>& send_std_array();
     
 //    //> \io_type READ_ARRAY param => 2*n_pts
 //    float* get_array(uint32_t n_pts);
-//    
 
     const char* get_cstr();
 
-    // -----------------------------------------------
     // Send integers
-    // -----------------------------------------------
-
     uint64_t read64();
     int read_int();
     unsigned int read_uint();
@@ -45,20 +38,18 @@ class Tests
         FAILED
     };
 
-//    #pragma is_failed
-//    bool IsFailed() const {return status == FAILED;}
+    # pragma is_failed
+    bool IsFailed() const {return status == FAILED;}
 
   private:
     int status;
     uint32_t waveform_size;
-    
     float mean;
     float std_dev;
   
     // Data buffers
     std::vector<float> data;
     std::array<uint32_t, 10> data_std_array;
-    
 }; // class Tests
 
 #endif // __TESTS_TESTS_HPP__
