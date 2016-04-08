3356722966cff1b3b706f51a808cc8ac8cc4c8bd
thomas <th.vanderbruggen@gmail.com>
2016-04-08 12:42:06 +0200

    Fix mistake

--- middleware/drivers/memory_map.cpp
+++ middleware/drivers/memory_map.cpp
@@ -52,7 +52,7 @@ int Klib::MemoryMap::Resize(uint32_t length)
     // printf("old virt addr = %lu\n", (uintptr_t)mapped_dev_base);
     // printf("new virt addr = %lu\n", (uintptr_t)new_virt_addr);
 
-    if (mapped_base == (void *) -1) {
+    if (new_virt_addr == (void *) -1) {
         fprintf(stderr, "Can't resize memory map.\n");
         return -1;
     }
