20894aabd887936b69689ebf26a1758b9581bc89
tvanderbruggen <th.vanderbruggen@gmail.com>
2016-05-05 23:31:01 +0100

    Clean up

--- devgen/autogen_implementation.py
+++ devgen/autogen_implementation.py
@@ -75,93 +75,30 @@ def PrintParseArg(file_id, device, operation):
     file_id.write('    return 0;\n')
     file_id.write('}\n\n')
     
-def PrintParserCore(file_id, device, operation):
-    arg_num = GetTotalArgNum(operation)
-    
-    if arg_num == 0:
+def PrintParserCore(file_id, device, operation):    
+    if GetTotalArgNum(operation) == 0:
         return
 
     file_id.write('    if (required_buffer_size<')
-    for idx, arg in enumerate(operation["arguments"]):
-        if idx < arg_num - 1:   
-            file_id.write(arg["type"] + ',')
-        else:
-            file_id.write(arg["type"])
+    PrintTypeList(file_id, operation)
     file_id.write('>() != cmd.payload_size) {\n')
     file_id.write("        kserver->syslog.print(SysLog::ERROR, \"Invalid payload size\\n\");\n")
     file_id.write("        return -1;\n")
     file_id.write("    }\n\n")
 
     file_id.write('    auto args_tuple = parse_buffer<0, ')
-    for idx, arg in enumerate(operation["arguments"]):
-        if idx < arg_num - 1:   
-            file_id.write(arg["type"] + ',')
-        else:
-            file_id.write(arg["type"])
+    PrintTypeList(file_id, operation)
     file_id.write('>(&cmd.buffer[0]);\n')
 
     for idx, arg in enumerate(operation["arguments"]):
         file_id.write('    args.' + arg["name"] + ' = ' + 'std::get<' + str(idx) + '>(args_tuple);\n');
 
-    
-    # #PrintArgNumTest(file_id, arg_num, device.class_name, operation)
-    # file_id.write("    char tmp_str[2*KSERVER_READ_STR_LEN];\n\n")
-    
-    # file_id.write("    uint32_t i = 0;\n")
-    # file_id.write("    uint32_t cnt = 0;\n")
-    # file_id.write("    uint32_t param_num = 0;\n\n")
-    
-    # file_id.write("    while(1) {\n")
-    # file_id.write("        if(cmd.buffer[i]=='\\0') {\n")
-    # file_id.write("            break;\n")
-    # file_id.write("        }\n")
-    # file_id.write("        else if(cmd.buffer[i]=='|') {\n")
-    # file_id.write("            tmp_str[cnt] = '\\0';\n\n")
-
-    # file_id.write("            switch(param_num)\n")
-    # file_id.write("            {\n")
-    
-    # for idx, arg in enumerate(operation["arguments"]):   
-    #     if "flag" in arg and arg["flag"] == 'CLIENT_ONLY':
-    #         continue
-                    
-    #     macro_name = GetConvMacro(arg["type"])
-        
-    #     if macro_name == 'Type error':
-    #         raise TypeError('\nIn device ' + device.class_name + ':\n' \
-    #                         + 'In operation ' + operation["name"] + ':\n' \
-    #                         + 'Invalid type for "' + arg["name"] \
-    #                                 + '" -> ' + arg["type"])
-                                    
-    #     file_id.write("              case " + str(idx) + ": {\n")
-            
-    #     if not "cast" in arg:
-    #         file_id.write("                  args." + arg["name"] +\
-    #                                  " = " + macro_name + "(tmp_str);\n")
-    #     else:
-    #         file_id.write("                  args." + arg["name"] +\
-    #                                  " = static_cast<" + arg["cast"] + ">("\
-    #                                      + macro_name + "(tmp_str));\n")
-            
-    #     file_id.write("                  break;\n")
-    #     file_id.write("              }\n")
-    
-    # file_id.write("            }\n\n")
-    
-    # file_id.write("            param_num++;\n")
-    # file_id.write("            cnt = 0;\n")
-    # file_id.write("            i++;\n")
-    # file_id.write("        } else {\n")
-    # file_id.write("            tmp_str[cnt] = cmd.buffer[i];\n")
-    # file_id.write("            i++;\n")
-    # file_id.write("            cnt++;\n")
-    # file_id.write("        }\n")
-    # file_id.write("    }\n\n")
-    
-    # file_id.write("    if(param_num == 0 || param_num > " + str(arg_num) + ") {\n")
-    # file_id.write("        kserver->syslog.print(SysLog::ERROR, \"Invalid number of parameters\\n\");\n")
-    # file_id.write("        return -1;\n")
-    # file_id.write("    }\n\n")
+def PrintTypeList(file_id, operation):
+    for idx, arg in enumerate(operation["arguments"]):
+        if idx < GetTotalArgNum(operation) - 1:   
+            file_id.write(arg["type"] + ',')
+        else:
+            file_id.write(arg["type"])
     
 def GetTotalArgNum(operation):
     if not dev_utils.IsArgs(operation):
