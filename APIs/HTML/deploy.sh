7fe2c9e57fc5d0cf9829bf15dab6a13fce391003
tvanderbruggen <th.vanderbruggen@gmail.com>
2016-05-08 14:52:28 +0100

    Remove HTML API

--- APIs/HTML/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python runserver.py
-
--- APIs/HTML/runserver.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-
-from App import app
-import os
-
-if __name__ == '__main__':
-    port_ = int(os.getenv('FLASK_PORT', 5000))
-    app.run(host='127.0.0.1', port=port_, debug=True)
