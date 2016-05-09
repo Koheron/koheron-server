eb0a1652af47e707613820534be9011259b32d8d
koheron-pc4 <th.vanderbruggen@gmail.com>
2016-05-09 19:33:11 +0200

    Start tests.coffee

--- APIs/js/koheron-websocket-client/gulpfile.js
+++ APIs/js/koheron-websocket-client/gulpfile.js
@@ -5,12 +5,18 @@ var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var closureCompiler = require('gulp-closure-compiler'); // Google closure compiler
 
-gulp.task('compile-js', function() {
+gulp.task('compile-lib-js', function() {
     gulp.src('src/*.coffee')
     .pipe(concat('koheron-websocket-client.coffee'))
     .pipe(coffee())
     .pipe(gulp.dest('./lib'))
     .pipe(concat('koheron-websocket-client.js'))
-    .pipe(closureCompiler('koheron-websocket-client.js'))
+    // .pipe(closureCompiler('koheron-websocket-client.js'))
     .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('compile-tests-js', function() {
+    gulp.src('tests/tests.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('./tests'));
 });
