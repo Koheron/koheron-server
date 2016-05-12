7811a4f6c312448fc7e0a02b18c446a5ac68af02
koheron-pc4 <th.vanderbruggen@gmail.com>
2016-05-12 23:04:19 +0200

    Fix dev status

--- APIs/python/setup.py
+++ APIs/python/setup.py
@@ -35,7 +35,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.0-alpha',
+    version='2.0.0',
 
     description='Client for Koheron TCP server',
     long_description=long_description,
@@ -56,7 +56,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Education',
