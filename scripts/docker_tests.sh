054c87ef3c4c8ce02aff562c3decd34a96a81777
tvanderbruggen <th.vanderbruggen@gmail.com>
2016-08-02 00:40:00 +0100

    Add finish function

--- scripts/docker_tests.sh
+++ scripts/docker_tests.sh
@@ -86,5 +86,9 @@ cd ../..
 echo "== Speed tests =="
 apis/C/tests/tests --speed 127.0.0.1:36000 /code/kserver.sock
 
-echo "== Server log =="
-cat server.log
+function finish {
+    echo "== Server log =="
+    cat server.log
+}
+
+trap finish EXIT
