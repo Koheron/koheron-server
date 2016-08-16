1578b68a47c0902db7b8556a82f053a2eadc7891
thomas <th.vanderbruggen@gmail.com>
2016-08-16 13:27:51 +0200

    Remove memory limits

--- core/devices_manager.cpp
+++ core/devices_manager.cpp
@@ -16,7 +16,7 @@ DeviceManager::DeviceManager(KServer *kserver_)
 : device_list(device_num)
 ,  kserver(kserver_)
 #if KSERVER_HAS_DEVMEM
-,  dev_mem(kserver_, kserver_->config->addr_limit_down, kserver_->config->addr_limit_up)
+,  dev_mem(kserver_)
 #endif
 {
     device_list[KSERVER] = static_cast<KDeviceAbstract*>(kserver);
