213ef23a1796c52d84575dc656aefc04aa2dc1ba
thomas <th.vanderbruggen@gmail.com>
2016-08-22 13:06:12 +0200

    Update hello world

--- apis/C/hello_world/hello_world.c
+++ apis/C/hello_world/hello_world.c
@@ -5,7 +5,7 @@ int main(void)
 {
     struct kclient *kcl = kclient_connect("127.0.0.1", 36000);
     dev_id_t id = get_device_id(kcl, "HelloWorld");
-    op_id_t add_42_ref = get_op_id(kcl, id, "ADD_42");
+    op_id_t add_42_ref = get_op_id(kcl, id, "add_42");
 
     uint32_t res;
     kclient_send_command(kcl, id, add_42_ref, "I", 58);
