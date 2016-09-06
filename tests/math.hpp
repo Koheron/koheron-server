597b275a0ff0fde3d9b091e06d3044a227e11019
Jean Minet <jean.minet@gmail.com>
2016-09-06 10:57:47 +0200

    replace HelloWorld by Math (hpp)

---
+++ tests/math.hpp
@@ -0,0 +1,13 @@
+
+#ifndef __MATH_HPP__
+#define __MATH_HPP__
+
+#include <cstdint>
+
+class Math
+{
+  public:
+    uint32_t add(uint32_t a, uint32_t b) {return a + b;}
+};
+
+#endif // __MATH_HPP__
