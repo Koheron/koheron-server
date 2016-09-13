8a4c95e84dfe8cdfff98173a125124392caff71d
thomas <th.vanderbruggen@gmail.com>
2016-09-13 18:35:23 +0200

    Rename variables to avoid collisions

--- scripts/build_run.mk
+++ scripts/build_run.mk
@@ -1,25 +1,30 @@
 # -------------------------------------------------------------------------------------
 # Build and run koheron-server in local
+#
+#
+# Usage: in your Makefile
+# KOHERON_SERVER_DEST = tmp
+# include build_run.mk
 # -------------------------------------------------------------------------------------
 
-SERVER_URL = https://github.com/Koheron/koheron-server.git
-SERVER_BRANCH = master
-SERVER_DIR = $(TMP)/koheron-server
-SERVER_BIN = $(SERVER_DIR)/tmp/kserverd
-SERVER_VENV = $(SERVER_DIR)/koheron_server_venv
+KOHERON_SERVER_URL = https://github.com/Koheron/koheron-server.git
+KOHERON_SERVER_BRANCH = master
+KOHERON_SERVER_DIR = $(KOHERON_SERVER_DEST)/koheron-server
+KOHERON_SERVER_BIN = $(KOHERON_SERVER_DIR)/tmp/kserverd
+KOHERON_SERVER_VENV = $(KOHERON_SERVER_DIR)/koheron_server_venv
 
-$(SERVER_DIR):
-	git clone $(SERVER_URL) $(SERVER_DIR)
-	cd $(SERVER_DIR) && git checkout $(SERVER_BRANCH)
+$(KOHERON_SERVER_DIR):
+	git clone $(KOHERON_SERVER_URL) $(KOHERON_SERVER_DIR)
+	cd $(KOHERON_SERVER_DIR) && git checkout $(KOHERON_SERVER_BRANCH)
 
-$(SERVER_DIR)/requirements.txt: $(SERVER_DIR)
+$(KOHERON_SERVER_DIR)/requirements.txt: $(KOHERON_SERVER_DIR)
 
-$(SERVER_VENV): $(SERVER_DIR)/requirements.txt
-	virtualenv $(SERVER_VENV)
-	$(SERVER_VENV)/bin/pip install -r $(SERVER_DIR)/requirements.txt
+$(KOHERON_SERVER_VENV): $(KOHERON_SERVER_DIR)/requirements.txt
+	virtualenv $(KOHERON_SERVER_VENV)
+	$(KOHERON_SERVER_VENV)/bin/pip install -r $(KOHERON_SERVER_DIR)/requirements.txt
 
-$(SERVER_BIN): $(SERVER_VENV)
-	make -C $(SERVER_DIR) CONFIG=config/config_local.yaml PYTHON=koheron_server_venv/bin/python
+$(KOHERON_SERVER_BIN): $(KOHERON_SERVER_VENV)
+	make -C $(KOHERON_SERVER_DIR) CONFIG=config/config_local.yaml PYTHON=koheron_server_venv/bin/python
 
-start_server: $(SERVER_BIN)
-	make -C $(SERVER_DIR) PYTHON=koheron_server_venv/bin/python start_server
+start_server: $(KOHERON_SERVER_BIN)
+	make -C $(KOHERON_SERVER_DIR) PYTHON=koheron_server_venv/bin/python start_server
