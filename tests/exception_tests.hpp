3b7cc9e79b0e2e6d7991514eff90f052094a267b
thomas <th.vanderbruggen@gmail.com>
2016-09-20 18:21:07 +0200

    Add exception tests

--- tests/exception_tests.hpp
+++ tests/exception_tests.hpp
@@ -1,10 +1,35 @@
 #ifndef __TESTS_EXCEPTION_TEST_HPP__
 #define __TESTS_EXCEPTION_TEST_HPP__
 
+#include <array>
+#include <vector>
+#include <tuple>
+
 class ExceptionTests
 {
   public:
-    auto ret_type_exception() {return true;}
+    auto ret_type_exception() {
+        return true;
+    }
+
+    std::array<float, 10>& std_array_type_exception() {
+        std_array.fill(0.0);
+        return std_array;
+    }
+
+    std::array<float, 10>& std_array_size_exception() {
+        std_array.fill(0.0);
+        return std_array;
+    }
+
+    std::vector<uint32_t>& std_vector_exception() {
+        std_vector.resize(10);
+        return std_vector;
+    }
+
+  private:
+    std::array<float, 10> std_array;
+    std::vector<uint32_t> std_vector;
 };
 
 #endif // __TESTS_EXCEPTION_TESTS_HPP__
--- tests/exception_tests.py
+++ tests/exception_tests.py
@@ -13,7 +13,19 @@ class ExceptionTests:
 
     @command()
     def ret_type_exception(self):
-        return self.client.recv_uint32()
+        return self.client.recv_uint32() # Instead of bool
+
+    @command()
+    def std_vector_exception(self):
+        return self.client.recv_vector(dtype='float32') # Instead of uint32
+
+    @command()
+    def std_array_type_exception(self):
+        return self.client.recv_array(shape=10, dtype='uint32') # Instead of float
+
+    @command()
+    def std_array_size_exception(self):
+        return self.client.recv_array(shape=20, dtype='float32') # Instead of 10
 
 port = int(os.getenv('PYTEST_PORT', '36000'))
 
@@ -24,3 +36,27 @@ def test_ret_type_exception(port):
     with pytest.raises(TypeError) as excinfo:
         tests.ret_type_exception()
     assert str(excinfo.value) == 'ExceptionTests::ret_type_exception returns a bool.'
+
+@pytest.mark.parametrize('port', [port])
+def test_std_vector_exception(port):
+    client = KoheronClient('127.0.0.1', port)
+    tests = ExceptionTests(client)
+    with pytest.raises(TypeError) as excinfo:
+        tests.std_vector_exception()
+    assert str(excinfo.value) == 'ExceptionTests::std_vector_exception expects elements of type uint32_t.'
+
+@pytest.mark.parametrize('port', [port])
+def test_std_array_type_exception(port):
+    client = KoheronClient('127.0.0.1', port)
+    tests = ExceptionTests(client)
+    with pytest.raises(TypeError) as excinfo:
+        tests.std_array_type_exception()
+    assert str(excinfo.value) == 'ExceptionTests::std_array_type_exception expects elements of type float.'
+
+@pytest.mark.parametrize('port', [port])
+def test_std_array_size_exception(port):
+    client = KoheronClient('127.0.0.1', port)
+    tests = ExceptionTests(client)
+    with pytest.raises(ValueError) as excinfo:
+        tests.std_array_size_exception()
+    assert str(excinfo.value) == 'ExceptionTests::std_array_size_exception expects 10 elements.'
