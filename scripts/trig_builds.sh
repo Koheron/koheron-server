69bea836f14694f7881fe43f4e751f4c6f29cc47
thomas <th.vanderbruggen@gmail.com>
2016-09-26 16:30:59 +0200

    Use for loop

--- scripts/trig_builds.sh
+++ scripts/trig_builds.sh
@@ -3,14 +3,10 @@ set -e
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-curl \
-  --header "Content-Type: application/json" \
-  --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
-  --request POST \
-  https://circleci.com/api/v1.1/project/github/Koheron/koheron-python/tree/master?circle-token=$CIRCLE_TOKEN
-
-curl \
-  --header "Content-Type: application/json" \
-  --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
-  --request POST \
-  https://circleci.com/api/v1.1/project/github/Koheron/automation/tree/master?circle-token=$CIRCLE_TOKEN
+for repo in koheron-python automation; do
+    curl \
+      --header "Content-Type: application/json" \
+      --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
+      --request POST \
+      https://circleci.com/api/v1.1/project/github/Koheron/$repo/tree/master?circle-token=$CIRCLE_TOKEN
+done
