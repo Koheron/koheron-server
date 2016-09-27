f52f77668a12d72afa72504cbdf583e70e690c64
thomas <th.vanderbruggen@gmail.com>
2016-09-27 12:54:48 +0200

    Fix variable name

--- scripts/trig_builds.sh
+++ scripts/trig_builds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-$koheron-python-branch=protocol
-$automation-branch=protocol
+$koheron_python_branch=protocol
+$automation_branch=protocol
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 
@@ -10,10 +10,10 @@ curl \
   --header "Content-Type: application/json" \
   --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
   --request POST \
-  https://circleci.com/api/v1.1/project/github/Koheron/koheron-python/tree/$koheron-python-branch?circle-token=$CIRCLE_TOKEN
+  https://circleci.com/api/v1.1/project/github/Koheron/koheron-python/tree/$koheron_python_branch?circle-token=$CIRCLE_TOKEN
 
 curl \
   --header "Content-Type: application/json" \
   --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
   --request POST \
-  https://circleci.com/api/v1.1/project/github/Koheron/automation/tree/$automation-branch?circle-token=$CIRCLE_TOKEN
+  https://circleci.com/api/v1.1/project/github/Koheron/automation/tree/$automation_branch?circle-token=$CIRCLE_TOKEN
