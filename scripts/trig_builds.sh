e2f6de67543758110f68ef8e45f3fa03deeb8726
thomas <th.vanderbruggen@gmail.com>
2016-09-27 12:48:26 +0200

    Specify branchs by hand

--- scripts/trig_builds.sh
+++ scripts/trig_builds.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e
 
+$koheron-python-branch=protocol
+$automation-branch=protocol
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-for repo in koheron-python automation; do
-    curl \
-      --header "Content-Type: application/json" \
-      --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
-      --request POST \
-      https://circleci.com/api/v1.1/project/github/Koheron/$repo/tree/$branch?circle-token=$CIRCLE_TOKEN
-done
+curl \
+  --header "Content-Type: application/json" \
+  --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
+  --request POST \
+  https://circleci.com/api/v1.1/project/github/Koheron/koheron-python/tree/$koheron-python-branch?circle-token=$CIRCLE_TOKEN
+
+curl \
+  --header "Content-Type: application/json" \
+  --data '{"build_parameters": {"KOHERON_SERVER_BRANCH": "'$branch'"}}' \
+  --request POST \
+  https://circleci.com/api/v1.1/project/github/Koheron/automation/tree/$automation-branch?circle-token=$CIRCLE_TOKEN
