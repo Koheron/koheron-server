6eb8ab0ccd235b23c6621833019b42aa2f4c58c5
thomas <th.vanderbruggen@gmail.com>
2016-10-07 19:11:28 +0200

    Create string function for parameter packs

--- core/kserver_syslog.hpp
+++ core/kserver_syslog.hpp
@@ -88,13 +88,11 @@ struct SysLog
     template<unsigned int severity, typename... Tp>
     void print(const char *message, Tp... args);
 
-    void print_dbg(const char *message, ...)
+    template<typename... Tp>
+    void print_dbg(const char *message,  Tp... args)
     {
-        if (config->verbose) {
-            va_list argptr;
-            va_start(argptr, message);
-            vprintf(message, argptr);
-        }
+        if (config->verbose)
+            printf_pack(message, args...);
     }
 
 private:
@@ -116,7 +114,7 @@ private:
         print_stdout<severity>(std::get<1>(log_array[severity]), std::string(message), args...);
 
         if (config->syslog)
-            syslog(std::get<0>(log_array[severity]), message, args...);
+            syslog_pack(std::get<0>(log_array[severity]), message, args...);
     }
 
     template<unsigned int severity, typename... Tp>
@@ -124,41 +122,10 @@ private:
     {
         if (severity <= WARNING) {
             auto fmt = std::string(header.data()) + ": " + message;
-            fprintf(stderr, fmt.data(), args...);
+            fprintf_pack(stderr, fmt.data(), args...);
         } else { // INFO
             if (config->verbose)
-                printf(message.data(), args...);
-        }
-
-        return 0;
-    }
-
-    template<unsigned int severity>
-    void notify(const char *message)
-    {
-        static constexpr std::array<std::tuple<int, str_const>, syslog_severity_num> log_array = {{
-            std::make_tuple(LOG_ALERT, str_const("KSERVER PANIC")),
-            std::make_tuple(LOG_CRIT, str_const("KSERVER CRITICAL")),
-            std::make_tuple(LOG_ERR, str_const("KSERVER ERROR")),
-            std::make_tuple(LOG_WARNING, str_const("KSERVER WARNING")),
-            std::make_tuple(LOG_NOTICE, str_const("KSERVER INFO"))
-        }};
-
-        print_stdout<severity>(std::get<1>(log_array[severity]), std::string(message));
-
-        if (config->syslog)
-            syslog(std::get<0>(log_array[severity]), "%s", message);
-    }
-
-    template<unsigned int severity>
-    int print_stdout(const str_const& header, const std::string& message)
-    {
-        if (severity <= WARNING) {
-            auto fmt = std::string(header.data()) + ": " + message;
-            fprintf(stderr, "%s", fmt.data());
-        } else { // INFO
-            if (config->verbose)
-                printf("%s", message.data());
+                printf_pack(message.data(), args...);
         }
 
         return 0;
@@ -167,8 +134,57 @@ private:
     template<unsigned int severity, typename... Tp>
     int emit_error(const char *message, Tp... args);
 
-    template<unsigned int severity>
-    int emit_error(const char *message);
+    // printf
+    template<typename... Tp>
+    typename std::enable_if_t< 0 < sizeof...(Tp), void >
+    printf_pack(const char *fmt, Tp... args) {
+        printf(fmt, args...);
+    }
+
+    template<typename... Tp>
+    typename std::enable_if_t< 0 == sizeof...(Tp), void >
+    printf_pack(const char *fmt, Tp... args) {
+        printf("%s", fmt);
+    }
+
+    // fprintf
+    template<typename... Tp>
+    typename std::enable_if_t< 0 < sizeof...(Tp), void >
+    fprintf_pack(FILE *stream, const char *fmt, Tp... args) {
+        fprintf(stream, fmt, args...);
+    }
+
+    template<typename... Tp>
+    typename std::enable_if_t< 0 == sizeof...(Tp), void >
+    fprintf_pack(FILE *stream, const char *fmt, Tp... args) {
+        fprintf(stream, "%s", fmt);
+    }
+
+    // snprintf
+    template<typename... Tp>
+    typename std::enable_if_t< 0 < sizeof...(Tp), int >
+    snprintf_pack(char *s, size_t n, const char *fmt, Tp... args) {
+        return snprintf(s, n, fmt, args...);
+    }
+
+    template<typename... Tp>
+    typename std::enable_if_t< 0 == sizeof...(Tp), int >
+    snprintf_pack(char *s, size_t n, const char *fmt, Tp... args) {
+        return snprintf(s, n, "%s", fmt);
+    }
+
+    // syslog
+    template<typename... Tp>
+    typename std::enable_if_t< 0 < sizeof...(Tp), void >
+    syslog_pack(int priority, const char *fmt, Tp... args) {
+        syslog(priority, fmt, args...);
+    }
+
+    template<typename... Tp>
+    typename std::enable_if_t< 0 == sizeof...(Tp), void >
+    syslog_pack(int priority, const char *fmt, Tp... args) {
+        syslog(priority, "%s", fmt);
+    }
 };
 
 } // namespace kserver
--- core/kserver_syslog.tpp
+++ core/kserver_syslog.tpp
@@ -22,26 +22,7 @@ void SysLog::print(const char *message, Tp... args)
 template<unsigned int severity, typename... Tp>
 int SysLog::emit_error(const char *message, Tp... args)
 {
-    int ret = snprintf(fmt_buffer, FMT_BUFF_LEN, message, args...);
-
-    if (ret < 0) {
-        fprintf(stderr, "emit_error: Format error\n");
-        return -1;
-    }
-
-    if (ret >= FMT_BUFF_LEN) {
-        fprintf(stderr, "emit_error: Buffer fmt_buffer overflow\n");
-        return -1;
-    }
-
-    kserver->pubsub.emit_cstr<PubSub::SYSLOG_CHANNEL, severity>(fmt_buffer);
-    return 0;
-}
-
-template<unsigned int severity>
-int SysLog::emit_error(const char *message)
-{
-    int ret = snprintf(fmt_buffer, FMT_BUFF_LEN, "%s", message);
+    int ret = snprintf_pack(fmt_buffer, FMT_BUFF_LEN, message, args...);
 
     if (ret < 0) {
         fprintf(stderr, "emit_error: Format error\n");
