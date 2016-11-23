38644d59774139a045b0491fbd57790fb108ed62
thomas <th.vanderbruggen@gmail.com>
2016-11-23 15:36:40 +0100

    Start context device

---
+++ tests/uses_context.hpp
@@ -0,0 +1,13 @@
+#ifndef __USES_CONTEXT_HPP__
+#define __USES_CONTEXT_HPP__
+
+#include <context.hpp>
+
+class UsesContext
+{
+  public:
+	UsesContext(Context& ct) {}
+
+};
+
+#endif // __USES_CONTEXT_HPP__
